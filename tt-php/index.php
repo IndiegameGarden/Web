5ba82b95db04f50743cfcba8be8f4f10d0dc4d2d
TranceTrance <info@trancetrance.com>
2013-12-06 15:14:45 +0100

    added trancetrance.com website

---
+++ tt-php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+	<meta content="text/html; charset=ISO-8859-1" http-equiv="Content-Type" />
+  <LINK href="igg.css" rel="stylesheet" type="text/css" />	
+  <link rel="shortcut icon" href="favicon.ico" />
+	<title>Trance Trance</title>
+</head>
+<body style="margin-left: 40px; font-family: Helvetica,Arial,sans-serif; font-size: 12pt; color: rgb(255, 255, 255); background-color: rgb(7, 3, 7);" alink="#CC66CC" link="#FFCCFF" vlink="#FF6666">
+
+<center>
+<img src="retro2.png" alt="Your view awaits here."/><br>
+</center>
+
+</body>
+</html>
