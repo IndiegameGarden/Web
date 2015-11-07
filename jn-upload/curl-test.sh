3c1291a4ecd0bcf5dbeecc4a0bff6035b251f4f5
IndiegameGarden <info@indiegamegarden.com>
2015-11-07 23:43:28 +0100

    fixed http post script

--- jn-upload/curl-test.sh
+++ jn-upload/curl-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-curl -A "Mozilla/5.0" http://jetnet.indiegamegarden.com/jn-upload.html
-
-#curl -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36" --form fileToUpload=@Astray.zip --form submit=Upload http://jetnet.indiegamegarden.com/jn-upload.html
+#curl -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36" http://jetnet.indiegamegarden.com/jn-upload.html
+curl 'http://jetnet.indiegamegarden.com/jetnet-upload.php' \
+  -A "Mozilla/5.0 (Windows NT 6.1)" \
+  -F "fileToUpload=@Astray.zip"
