68762b95261fec473bce9fc79c0f94e941dfd90e
IndiegameGarden <info@indiegamegarden.com>
2015-12-02 21:37:46 +0100

    bugfix ampersand, newlines added

--- jn-upload/game-upload.sh
+++ jn-upload/game-upload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # put game html5 into a zip file
-zip -r ~/game.zip ~/Desktop/Game/BreakIt/Export/html5/bin/*
+#zip -r ~/game.zip ~/Desktop/Game/BreakIt/Export/html5/bin/*
 
 # upload zip to server
 curl 'http://jetnet.indiegamegarden.com/jetnet-upload.php' \
   -A "Mozilla/5.0 (Windows NT 6.1)" \
-  -F "fileToUpload=game.zip"
+  -F "fileToUpload=@game.zip"
--- jn-upload/jetnet-upload.php
+++ jn-upload/jetnet-upload.php
@@ -1,4 +1,5 @@
 <?php
+echo "jetnet-upload.php v1.0<br>\n" ;
 $target_dir = "game-uploads/"; // where new zip files go
 $game_dir   = "game-store/"; // where unzipped files are hosted
 
@@ -7,29 +8,29 @@ $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 $id = pathinfo($target_file,PATHINFO_FILENAME);
 $game_dir = $game_dir . $id ;
-echo "Uploading id=$id <br>" ;
+echo "Uploading id=$id <br>\n" ;
 
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 8000000) {
-    echo "Sorry, your file is too large.<br>";
+if ($_FILES["fileToUpload"]["size"] > 18000000) {
+    echo "Sorry, your file is too large.<br>\n";
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "zip" && $imageFileType != "ZIP" ) {
-    echo "Sorry, wrong file formatz.<br>";
+    echo "Sorry, wrong file format uploaded.<br>\n";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Result -> Your file was not uploaded.<br>";
+    echo "Result -> Your file was not uploaded.<br>\n";
 } else {
 // if everything is ok, try to upload file
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
+        echo basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>\n";
         // unzip
         $file = $target_file;
         if (! is_dir( $game_dir ) ) {
-          echo "Creating folder $game_dir <br>" ;
+          echo "Creating folder $game_dir <br>\n" ;
           mkdir($game_dir);
         }
         $zip = new ZipArchive;
@@ -38,12 +39,12 @@ if ($uploadOk == 0) {
           // extract it to the path we determined above
           $zip->extractTo($game_dir);
           $zip->close();
-          echo "File $file extracted to $game_dir<br>";
+          echo "File $file extracted to $game_dir<br>\n";
         } else {
-          echo "Result -> Error, I couldn't open $file for unzipping.<br>";
+          echo "Result -> Error, I couldn't open $file for unzipping.<br>\n";
         }
     } else {
-        echo "Result -> there was an error uploading your file.<br>";
+        echo "Result -> there was an error uploading your file.<br>\n";
     }
 }
 ?>
