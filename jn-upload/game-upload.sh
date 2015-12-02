228904f13e352471431024dbe0628373480fca42
IndiegameGarden <info@indiegamegarden.com>
2015-12-02 22:14:15 +0100

    added random id to script; zip to avoid deep paths; URL display

--- jn-upload/game-upload.sh
+++ jn-upload/game-upload.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
+cd ~
+echo "Jet-Net Bouw Je Eigen Game - game uploader."
+
+# get/generate the student's id for game
+date=$(date +%Y%m%d)
+if [ -f studentid ]
+then
+  studentid=$(cat studentid)
+else
+  studentid=$(cat /dev/urandom | tr -cd [:alnum:] | head -c 4)
+  echo "$studentid" > studentid
+fi
+ID="$date$studentid"
+fn="$ID.zip"
+
 # put game html5 into a zip file
-#zip -r ~/game.zip ~/Desktop/Game/BreakIt/Export/html5/bin/*
+rm -f $fn
+pushd ~/Desktop/Game/BreakIt/Export/html5/bin
+zip -r ~/$fn *
+popd
 
 # upload zip to server
+echo "Uploading file to web server... just a moment"
 curl 'http://jetnet.indiegamegarden.com/jetnet-upload.php' \
   -A "Mozilla/5.0 (Windows NT 6.1)" \
-  -F "fileToUpload=@game.zip"
+  -F "fileToUpload=@$fn"
+
+echo " "
+echo "------------------------------------------------"
+echo "Game is online on:"
+echo "http://jetnet.indiegamegarden.com/game-store/$ID"
+echo "------------------------------------------------"
+read -p "Press [ENTER] to exit."
